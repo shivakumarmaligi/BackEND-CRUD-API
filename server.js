c330fd24e08053ca1bbe9a67056b8f873d5922ac
Shivakumar MR <shivakumarmaligi@gmail.com>
2022-06-07 00:19:15 +0530

    CRUD API using Express JS

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const { PORT, NODE_ENV } = require("./config");
+const { DBConnection } = require("./config/db");
+const StudentRoutes = require("./routes/student");
+const app = express();
+
+let StartServer = async () => {
+  /*---------------database connection ---------------------*/
+  DBConnection();
+  /*---------------end database connection ------------------*/
+
+  /*------------------Middleware section start here --------------*/
+  if (NODE_ENV === "development") {
+    app.use(morgan("dev"));
+  }
+  app.use(express.json());
+  app.use(cors());
+  /*------------------Middleware section end here --------------*/
+
+  /*-----------load routes--------------------*/
+  app.use("/api/students/", StudentRoutes);
+  /*-----------load routes ended--------------------*/
+  /*-------------listen port ------------------------------*/
+  app.listen(PORT, err => {
+    if (err) throw err;
+    console.log(`server is running on port number ${PORT}`);
+  });
+  /*-------------listen port ended here ------------------------------*/
+};
+
+StartServer();
